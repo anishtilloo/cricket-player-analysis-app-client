dbd2679e9450c0cd609befad1ee762b27c3384ba
Anish Tilloo <anishtilloo@gmail.com>
2024-01-07 19:39:30 +0530

    integrated team api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import './App.css'
+import { useEffect, useState } from "react";
+
+type Coach = {
+  name: string;
+  experience: number;
+}
+
+type Team = {
+  id: number;
+  teamName: string;
+  ownerName: string;
+  coach: Coach;
+  netWorth: number;
+}
+
+type ResponseTeam = {
+  success: boolean;
+  message: string;
+  data: Team[];
+}
 
 function App() {
-
+   const [teams, setTeams] = useState<ResponseTeam>();
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/get-all-teams")
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      setTeams(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+  }, [])
+  console.log(teams);
+  
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="bg-[#0f0f0f] h-[100vh] text-[#ffffff] text-xl">
         Hello world!
+        {teams?.data.map((team) => (
+          <div key={team.id}>
+            <p>{team.teamName}</p>
+          </div>
+        ))}
       </h1>
     </>
   )

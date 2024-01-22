31e4a0dbe29e5df4cfc05eff538faea824a30546
Anish Tilloo <anish.tilloo@claimgenius.com>
2024-01-22 15:18:36 +0530

    integrated the player api with the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { ResponseTeam } from "./types/team.types";
+import { ResponsePlayer } from "./types/player.types";
 
 
 function App() {  
   const apiUrl = import.meta.env.VITE_API_URL;
    const [teams, setTeams] = useState<ResponseTeam>();
+   const [players, setPlayers] = useState<ResponsePlayer>();
   useEffect(() => {
     fetch(`${apiUrl}/get-all-teams`)
     .then((response) => response.json())
@@ -15,17 +17,36 @@ function App() {
     .catch((err) => {
       console.log(err.message);
     })
+
+    fetch(`${apiUrl}/get-all-palyers`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      setPlayers(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
   }, [apiUrl]);
   
   return (
     <>
-      <h1 className="bg-[#0f0f0f] h-[100vh] text-[#ffffff] text-xl">
-        {teams?.data.map((team) => (
-          <div key={team.id}>
-            <p>{team.teamName}</p>
-          </div>
-        ))}
-      </h1>
+      <div className="bg-[#0f0f0f] h-[100vh] flex justify-evenly">
+        <div className="text-[#ffffff] text-md">
+          {teams?.data.map((team) => (
+            <div key={team.id}>
+              <p>{team.teamName}</p>
+            </div>
+          ))}
+        </div>
+        <div className="text-[#ffffff] text-md">
+          {players?.data.map((player) => (
+            <div key={player.id}>
+              <p>{player.playerName}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/types/player.types.ts
@@ -0,0 +1,23 @@
+export type Player = {
+    id?: number;
+    playerName: string;
+    physicals: unknown;
+    mentalStats: unknown;
+    characteristics: unknown;
+    height: number;
+    weight: number;
+    basePrise: number;
+    actualPrise: number;
+    injured: unknown;
+    fitnessScore: number,
+    analysis: unknown,
+    playerType: unknown,
+    teamId: number,
+    teamName: string,
+}
+
+export type ResponsePlayer = {
+    success: boolean;
+    message: string;
+    data: Player[];
+}
